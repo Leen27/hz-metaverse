7794bdf18fb64dd72ca1207ece20e958197214e1
linjq <m13840918032@163.com>
2023-03-28 01:28:13 +0800

    feat(cesium)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import * as Cesium from 'cesium';
+import "cesium/Build/Cesium/Widgets/widgets.css";
+(window as any).CESIUM_BASE_URL = '/Cesium';
+
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer', {})
+})
 </script>
 
 <template>
-  <main>
-    <div class="cesiumContainer"></div>
+  <main class="absolute flex items-center left-0 top-0 w-full h-screen" >
+    <div class="absolute left-0 top-0 w-full h-screen"  id="cesiumContainer"></div>
   </main>
 </template>
